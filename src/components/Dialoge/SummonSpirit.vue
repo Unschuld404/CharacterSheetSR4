2d5724e20038014177f7204f2655a26ec939d9eb
Unschuld404 <git@unschuld.org>
2024-10-20 18:26:48 +0200

    Dialog für Geister beschwören erstellt

--- src/components/Dialoge/SummonSpirit.vue
+++ src/components/Dialoge/SummonSpirit.vue
@@ -10,8 +10,35 @@ const dialogeStore = useDialogeStore();
 
   <div class="modal-overlay">
     <div class="modal-content">
-      <h1>SummonSpirit</h1>
-      <button @click="dialogeStore.hide">OK</button>
+
+      <div class="column">
+        <select class="row">
+          <option>Erdgeist</option>
+          <option>Feuergeist</option>
+          <option>Luftgeist</option>
+          <option>Wassergeist</option>
+          <option>Geist des Menschen</option>
+          <option>Geist des Tieres</option>
+          <option>Watcher</option>
+        </select>
+
+        <div class="row">
+          <div class="force">Kraft</div>
+          <i class='bx bxs-minus-square'></i>
+          <div class="value">3</div>
+          <i class='bx bxs-plus-square'></i>
+        </div>
+
+        <div class="row">
+          <div class="services">Dienste</div>
+          <i class='bx bxs-minus-square'></i>
+          <div class="value">3</div>
+          <i class='bx bxs-plus-square'></i>
+        </div>
+
+      </div>
+
+      <button class="confirm" @click="dialogeStore.hide">Beschwören</button>
 
     </div>
   </div>
@@ -20,6 +47,49 @@ const dialogeStore = useDialogeStore();
 
 <style scoped>
 
+.modal-content {
+  width: 50vh;
+  height: 40vh;
+}
 
+.column {
+  height: 30vh;
+  justify-content: space-around;
+}
+
+select {
+  background-color: transparent;
+  border: none;
+  color: var(--accent-color);
+}
+
+option {
+  background-color: var(--primary-color);
+  align-items: center;
+}
+
+.row {
+  height: 8vh;
+  line-height: 8vh;
+  font-size: 4vh;
+  align-items: center;
+  gap: 2vh;
+}
+
+i {
+  font-size: 6vh;
+  width: 10vh;
+  text-align: center;
+}
+
+.value {
+  width: 10vh;
+  text-align: center;
+  font-weight: bold;
+}
+
+.force, .services {
+  width: 20vh;
+}
 
 </style>/
--- src/views/Character.vue
+++ src/views/Character.vue
@@ -3,11 +3,12 @@ import RollDice from "@/components/Dialoge/RollDice.vue";
 import ChummerUnknown from "@/components/Dialoge/ChummerUnknown.vue";
 import ManageEdge from "@/components/Dialoge/ManageEdge.vue";
 import ChangeNuyen from "@/components/Dialoge/ChangeNuyen.vue";
+import SummonSpirit from "@/components/Dialoge/SummonSpirit.vue";
 </script>
 
 <template>
 
-  <RollDice />
+  <SummonSpirit />
 
 </template>
 
