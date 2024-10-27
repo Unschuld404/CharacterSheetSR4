68b63387ae1b03d1626784ba1fb2945bfa2fe0d7
Unschuld404 <git@unschuld.org>
2024-10-27 02:32:10 +0200

    ...

--- src/components/Dialoge/SpiritSheet.vue
+++ src/components/Dialoge/SpiritSheet.vue
@@ -1,24 +1,167 @@
 <script setup lang="ts">
-
 import {DialogRollDice, DialogSpiritSheet} from "@/composables/dialogs";
 </script>/
 
 <template>
 
   <div v-if="DialogSpiritSheet.visible" class="modal-overlay" @click="DialogSpiritSheet.hide">
-    <div class="modal-content">
+    <div class="modal-content" @click.stop>
+
+      <div class="plane row">
+        <div>Aktuelle Ebene:</div>
+        <div class="toggle">Toggle</div>
+      </div>
+
+      <h1 class="critter-name">Watcher Stufe 1</h1>
 
       <div class="row">
-        <div class="critter-name bob header">Watcher</div>
-        <div class="kraft bob total-value" @click="DialogRollDice.show">1</div>
+
+        <div class="scroll-box">
+
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+          <div class="item row">
+            <div class="skill">Fertigkeit/Kraft</div>
+            <div class="total-value">5</div>
+          </div>
+
+
+        </div>
+
+        <div class="column">
+
+          <div class="row">
+
+
+
+          </div>
+
+        </div>
+
       </div>
-      <div class="attributes">Körperliche Attribute</div>
+
     </div>
   </div>
 
 </template>
 
 <style scoped>
+
+.item {
+  font-size: 3vh;
+  line-height: 5vh;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.total-value {
+  line-height: 4vh;
+}
+
+.scroll-box {
+  width: 40%;
+  height: 65vh;
+}
+
+.plane {
+  position: absolute;
+  left: 20%;
+  transform: translateX(-50%);
+  top: 4vh;
+  width: 30%;
+  text-align: center;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.toggle {
+  left: 20%;
+  background-color: var(--background-color);
+  padding: 1vh;
+  border-radius: 1vh;
+  width: 40%;
+  text-align: center;
+}
+
+.toggle:active {
+  background-color: transparent;
+  color: var(--accent-color);
+  border: 1px solid var(--accent-color);
+}
 
 .modal-overlay {
   z-index: 1000;
@@ -28,6 +171,7 @@ import {DialogRollDice, DialogSpiritSheet} from "@/composables/dialogs";
   height: 80vh;
   width: 80vw;
   z-index: 1001;
+  position: relative;
 }
 
 </style>/
