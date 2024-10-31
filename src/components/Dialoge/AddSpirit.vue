31c45db815dabc63b52e488b72499ec625b625ca
dj <dj.atlassian@cura-go.de>
2024-10-31 18:56:42 +0100

    addSpirit rework

--- src/components/Dialoge/AddSpirit.vue
+++ src/components/Dialoge/AddSpirit.vue
@@ -14,8 +14,7 @@ const spiritTypes = [
   'Watcher'
 ];
 
-const showList = ref(true);
-const selectedType = ref(null);
+const selectedType = ref('Watcher');
 const force = ref(10);
 const services = ref(1);
 
@@ -23,7 +22,7 @@ force.value = char.attributes.magic.total;
 
 const choose = (type: any) => {
   selectedType.value = type;
-  showList.value = false;
+  DialogAddSpirit.doChoose = false;
 }
 
 function addForce()
@@ -55,7 +54,6 @@ function addSpirit()
 {
   char.addSpirit(selectedType.value ?? 'Spirit', force.value, services.value);
   DialogAddSpirit.hide();
-  showList.value = true;
 }
 
 </script>
@@ -63,49 +61,110 @@ function addSpirit()
 <template>
 
   <div v-if="DialogAddSpirit.visible" class="modal-overlay" @click="DialogAddSpirit.hide()">
-    <div class="modal-content" @click.stop>
 
-      <template v-if="showList">
-        <div class="column">
-          <div  v-for="(item, index) in spiritTypes"
-                :key="index"
-                :class="['type', {active: selectedType === index }]"
-                @click="choose(item)"
-          >
-            {{ item }}
-          </div>
+
+
+    <div v-if="DialogAddSpirit.doChoose" class="modal-content-choose" @click.stop>
+      <div class="column-choose">
+        <div  v-for="(item, index) in spiritTypes"
+              :key="index"
+              :class="['type', {active: selectedType === item }]"
+              @click="choose(item)"
+        >
+          {{ item }}
         </div>
-      </template>
+      </div>
+    </div>
 
-      <template v-if="!showList">
-        <h1>{{ selectedType ?? 'Spirit' }}</h1>
 
-        <div class="column">
-          <div class="row">
-            <div class="force">Kraft</div>
-            <div class="mutator" @click="substractForce">-</div>
-            <div class="value">{{ force }}</div>
-            <div class="mutator" @click="addForce">+</div>
-          </div>
+    <div v-if="!DialogAddSpirit.doChoose" class="modal-content" @click.stop>
+      <h1>{{ selectedType ?? 'Spirit' }}</h1>
 
-          <div class="row">
-            <div class="services">Dienste</div>
-            <div class="mutator" @click="substractServices">-</div>
-            <div class="value">{{ services }}</div>
-            <div class="mutator" @click="addServices">+</div>
-          </div>
-
+      <div class="column">
+        <div class="row">
+          <div class="force">Kraft</div>
+          <div class="mutator" @click="substractForce">-</div>
+          <div class="value">{{ force }}</div>
+          <div class="mutator" @click="addForce">+</div>
         </div>
 
-        <button class="confirm" @click="addSpirit()">Hinzufügen</button>
-      </template>
+        <div class="row">
+          <div class="services">Dienste</div>
+          <div class="mutator" @click="substractServices">-</div>
+          <div class="value">{{ services }}</div>
+          <div class="mutator" @click="addServices">+</div>
+        </div>
 
+      </div>
+
+      <button class="confirm" @click="addSpirit()">Hinzufügen</button>
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
+
+.modal-content-choose {
+  width: 55vh;
+  height: 75vh;
+  z-index: 1001;
+}
+
+.modal-content {
+  width: 50vh;
+  height: 45vh;
+  z-index: 1001;
+}
+
+.modal-overlay {
+  z-index: 1000;
+}
+
+.column-choose {
+  margin-top: 1vh;
+  height: 100%;
+  justify-content: space-evenly;
+}
+
+.column {
+  height: 30vh;
+  justify-content: space-around;
+}
+
+.row {
+  height: 8vh;
+  line-height: 8vh;
+  font-size: 4vh;
+  align-items: center;
+  gap: 2vh;
+}
+
+i {
+  font-size: 6vh;
+  width: 10vh;
+  text-align: center;
+}
+
+.value {
+  width: 10vh;
+  text-align: center;
+  font-weight: bold;
+}
+
+.force, .services {
+  width: 20vh;
+}
+
+.type {
+  appearance: none;
+  text-align: center;
+  font-size: 3vw;
+  color: var(--accent-color);
+  line-height: 7vh;
+  background-color: var(--background-color);
+  border: none;
+  border-radius: 1vh;
+  margin-bottom: 2vh;
+}
 
 </style>
