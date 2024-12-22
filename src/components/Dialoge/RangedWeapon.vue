68de9122dc873716db0bf0fe641915fa4148888e
Unschuld404 <git@unschuld.org>
2024-12-22 17:07:17 +0100

    waffendialog ist jetzt mobil

--- src/components/Dialoge/RangedWeapon.vue
+++ src/components/Dialoge/RangedWeapon.vue
@@ -72,98 +72,77 @@ function shoot()
 
   <div v-if="DialogWeapon.visible" class="modal-overlay" @click="DialogWeapon.hide">
     <div class="modal-content" @click.stop>
-
-      <div class="the-weapon">
-
-        <div class="row box name"><h1>{{ weapon.name }}</h1></div>
-        <div class="row">
-          <div v-if="weapon.mods.length > 0" class="column box">
-            <div class="lower-header">
-              Waffen-Mods
-            </div>
-            <div v-for="mod in weapon.mods" class="line">
-              {{ mod.name }}
-              <template v-if="mod.rating > 0">( {{ mod.rating }} )</template>
-              <template v-if="mod.rc != 0">- RC: ( {{ mod.rc }} )</template>
-            </div>
-          </div>
-          <div class="column">
-            <div class="shooting-mode">
-              <RadioButtons class="mode" v-model="selectShootingMode" :options="shootingMode" group="modes" id="mode"/>
-            </div>
-          </div>
-          <div class="column divers">
-            <div class="row reach">
-              <RadioButtons class="mode" v-model="selectReach" :options="ranges" group="distances" id="reach"/>
-            </div>
-            <div v-if="isLoaded()" class="row" >
-              {{ setting.ammoLoaded }} {{ setting.ammoLeft }} / {{ setting.magSize }} ({{ setting.magType }})
-              <button @click="reload">reload</button>
-              <button @click="load">switch ammo</button>
-            </div>
-            <div v-else class="row empty" >
-              <button @click="load">Munition laden</button>
-            </div>
-            <div class="row">
-              <div class="column box narrow">
-                <div class="lower-header">
-                  Schaden
-                </div>
-                <div class="mod">
-                  {{ weapon.damage }}
-                </div>
-              </div>
-              <div class="column box narrow">
-                <div class="lower-header">
-                  Pool
-                </div>
-                <div class="mod">
-                  {{ weapon.dicepool }}
-                </div>
-              </div>
-              <div class="column box wide">
-                <div class="row-info">
-                  <p>Panzerbrechend</p>
-                  <strong>{{ weapon.ap }}</strong>
-                </div>
-                <div class="row-info">
-                  <p>Rückstoßkompensation</p>
-                  <strong>{{ weapon.rc }}</strong>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="column box narrow">
-                <div class="lower-header">
-                  Distanz-Mod
-                </div>
-                <div class="mod">
-                  {{ rangeModifier }}
-                </div>
-              </div>
-              <div class="column box narrow">
-                <div class="lower-header">Modus-Mod</div>
-                <div class="mod">
-                  -{{ modeModifier }}
-                </div>
-              </div>
-
-              <button class="dice wide" @click="DialogRollDice.setValues(
-              {
-                name: weapon.name,
-                value: toInt(weapon.dicepool) + rangeModifier - modeModifier,
-                values: [
-                    {name: 'Fertigkeit', value: toInt(weapon.dicepool)-char.attributes.agility.total},
-                    {name: 'Geschicklichkeit', value: char.attributes.agility.total},
-                    {name: 'Distanz', value: rangeModifier},
-                    {name: 'Modus', value: -modeModifier},
-                    ]
-              }
-              ).show()">{{ toInt(weapon.dicepool) + rangeModifier - modeModifier }}
-              </button>
-            </div>
+      <h1>{{ weapon.name }}</h1>
+      <div class="column">
+        <div class="item">
+          <div>Schaden</div>
+          <div>{{ weapon.damage }}</div>
+        </div>
+        <div class="item">
+          <div>Pool</div>
+          <div>{{ toInt(weapon.dicepool) + rangeModifier - modeModifier }}</div>
+        </div>
+        <div class="item">
+          <div>Panzerbrechend</div>
+          <div>{{ weapon.ap }}</div>
+        </div>
+        <div class="item">
+          <div>Rückstoßkompensation</div>
+          <div>{{ weapon.rc }}</div>
+        </div>
+        <div v-if="weapon.mods.length > 0">
+          <div v-for="mod in weapon.mods" class="item">
+            {{ mod.name }}
+            <template v-if="mod.rating > 0">( {{ mod.rating }} )</template>
+            <template v-if="mod.rc != 0">- RC: ( {{ mod.rc }} )</template>
           </div>
         </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <strong>Distanz-Mod</strong>
+          <div>{{ rangeModifier }}</div>
+          <div>Entfernung</div>
+          <RadioButtons class="mode" v-model="selectReach" :options="ranges" group="distances"/>
+        </div>
+        <div class="column">
+          <strong>Modus-Mod</strong>
+          <div>-{{ modeModifier }}</div>
+          <div>Schussmodus</div>
+          <RadioButtons class="mode" v-model="selectShootingMode" :options="shootingMode" group="modes"/>
+        </div>
+      </div>
+      <div v-if="isLoaded()" class="ammo">
+        {{ setting.ammoLoaded }} {{ setting.ammoLeft }} / {{ setting.magSize }} ({{ setting.magType }})
+      </div>
+
+
+      <div class="row">
+
+        <div v-if="isLoaded()">
+          <button @click="load">switch ammo</button>
+        </div>
+        <div v-if="isLoaded()">
+          <button @click="reload">reload</button>
+        </div>
+        <div v-else class="empty" >
+          <button @click="load">Munition laden</button>
+        </div>
+        <button>schiessen</button>
+
+        <button @click="DialogRollDice.setValues(
+      {
+        name: weapon.name,
+        value: toInt(weapon.dicepool) + rangeModifier - modeModifier,
+        values: [
+            {name: 'Fertigkeit', value: toInt(weapon.dicepool)-char.attributes.agility.total},
+            {name: 'Geschicklichkeit', value: char.attributes.agility.total},
+            {name: 'Distanz', value: rangeModifier},
+            {name: 'Modus', value: -modeModifier},
+            ]
+      }
+      ).show()">({{ toInt(weapon.dicepool) + rangeModifier - modeModifier }}) Würfel
+        </button>
 
       </div>
 
@@ -174,85 +153,28 @@ function shoot()
 
 <style scoped>
 
-.line {
+button {
   height: 6vh;
-  align-content: center;
-  border-bottom: 1px solid var(--background-color)
-}
-.line:last-child{
-  border: none;
-}
-
-.row-info {
-  display: flex;
-  justify-content: space-between;
-  font-size: 2vh;
-  line-height: 3vh;
-}
-
-.mod {
+  font-size: 4vw;
   font-weight: bold;
-  font-size: 3vh;
-  line-height: 7vh;
+  padding-left: 3vw;
+  padding-right: 3vw;
+  margin-top: 1vh;
 }
 
-.dice {
-  padding: 1vh;
-  font-size: 5vh;
-}
-
-.narrow {
-  width: 15vh;
-  height: 10vh;
-  flex: 1;
+.ammo {
+  margin-top: 2vh;
   text-align: center;
 }
 
-.wide {
-  flex: 3;
+.item {
+  height: 4vh;
+  border-bottom: 1px solid var(--font-color);
 }
 
-.empty {
-  width: 100%;
-  flex: 1;
-}
-
-button{
-  width: 100%;
-  height: 100%;
-  font-size: 3vh;
-  font-weight: bold;
-  color: var(--accent-color);
-  background-color: var(--background-color);
-  border-radius: 1vh;
-  border: 1px solid var(--font-color)
-}
-
-.reach {
-  flex: 1;
-  border: 1px solid var(--font-color);
-  border-radius: 1vh;
-}
-
-.divers {
-  flex: 3;
-}
-
-.row {
-  gap: 2vh;
-}
-
-.column {
-  gap: 2vh;
-}
-
-.name {
+.item:last-child {
   margin-bottom: 2vh;
-}
-
-.the-weapon {
-  display: flex;
-  flex-direction: column;
+  border-bottom: none;
 }
 
 .modal-overlay {
@@ -264,19 +186,27 @@ button{
   background-color: transparent;
   border: none;
   z-index: 2001;
+  max-height: 75vh;
+  overflow: scroll;
 }
 
-#mode.radio {
+::v-deep(.radio) {
   flex-direction: column;
-  height: 38vh;
-  width: 20vh;
   border: 1px solid var(--font-color);
+  width: 44vw;
 }
 
-#reach.radio {
-  width: 100%;
+::v-deep(.radio_label) {
+  height: 5vh;
 }
 
+.column {
+  gap: 1vh;
+}
 
+.row {
+  gap: 1vh;
+  justify-content: space-between;
+}
 
 </style>/
