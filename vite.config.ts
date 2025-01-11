7fe0588efa71161b751a817599a5a1bf3b61aaed
dj <dj.atlassian@cura-go.de>
2025-01-11 15:47:59 +0100

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,17 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Für JS/CSS-Chunks
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        // Für andere Assets (Bilder, Fonts etc.)
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
